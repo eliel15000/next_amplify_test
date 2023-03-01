9e2b83594e9bc49c93edb5aa8b171a10837137d7
eliel15000 <eliezercoding@gmail.com>
2023-02-28 22:28:50 -0500

    next commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,30 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import React, { useState } from "react"
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const [recipes, setRecipes] = useState([]);
+  const [query, setQuery] = useState("banana");
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${process.env.NEXT_PUBLIC_APP_ID}&app_key=${process.env.NEXT_PUBLIC_APP_KEY}`
+    );
+    const data = await response.json();
+    // console.log(data.hits);
+    setRecipes(data.hits);
+    console.log(data.hits);
+  };
+
+  // const helloF = () => {
+  //   console
+  // }
+
   return (
     <>
       <Head>
@@ -114,6 +133,21 @@ export default function Home() {
             <p className={inter.className}>
               Instantly deploy your Next.js site to a shareable URL
               with&nbsp;Vercel.
+            </p>
+          </a>
+
+          <a
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={getRecipes}
+          >
+            <h2 className={inter.className}>
+              Docs <span>-&gt;</span>
+            </h2>
+            <p className={inter.className}>
+              Find in-depth information about Next.js features and&nbsp;API.
             </p>
           </a>
         </div>
